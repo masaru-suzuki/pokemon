6e324965186604a333e9e7b2aaf1218095b7b91b
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2019-11-25 15:52:43 +0900

    HPのアニメーション以外完了

--- js/main.js
+++ js/main.js
@@ -8,17 +8,6 @@
 先にHPが無くなったほうが負け
 注意点:ターン制にするので、そのキャラのターンでないと攻撃ボタンを押せない
 -------------------------------------------*/
-//character
-const enemy = {name:'ギャラドス', hp:120, lv:80};
-const player = {name:'ピカチュウ', hp:100, lv:70};
-
-//damege
-//level差でダメージ幅変えたい
-const minDamege = 5;
-const maxDamege = 20;
-const damege = minDamege + Math.floor(Math.random() * (maxDamege + 1 -minDamege));
-
-
 const enemyGauge = document.getElementById('enemyGauge');
 const enemyHp = document.getElementById('enemyHp');
 const enemyAttackBtn = document.getElementById('enemyAttackBtn');
@@ -26,54 +15,172 @@ const playerGauge = document.getElementById('playerGauge');
 const playerHp = document.getElementById('playerHp');
 const playerAttackBtn = document.getElementById('playerAttackBtn');
 
-//startStatus
-  //Enemy
-  // let leftEnemyHp = enemy.hp
-  enemyHp.textContent = enemy.hp + '/'+ enemy.hp
-  //Enemy Hp Gauge
-  enemyGauge.classList.add('enemy-hp-gauge');
-  //Player
-  let leftPlayerHp = player.hp
-  playerHp.textContent = leftPlayerHp + '/'+ player.hp
+//enemy
+const enemy = {name:'ギャラドス', hp:120, lv:80, remainingHp: 120};
+let remainEnemyHpPercentage;
+enemyHp.textContent = enemy.hp + '/'+ enemy.hp;
+enemyGauge.classList.add('enemy-gauge');
+//player
+const player = {name:'ピカチュウ', hp:100, lv:70, remainingHp: 100};
+let remainPlayerHp = player.hp
+let remainPlayerHpPercentage;
+playerHp.textContent = player.hp + '/'+ player.hp
+playerGauge.classList.add('player-gauge');
+
+//Initial
+let damage;
+playerAttackBtn.disabled = true;
 
 
-//function
-const attackDamege = () => {
-  leftEnemyHp = enemy.hp - damege;
-  //  leftEnemyHp = enemy.hp -= damege;
-  //  にすると、最大Hpの表示がleftEnemyHpになる。なんで？
 
+
+
+//////////////////////////function/////////////////////////////
+const start = () => {
+  enemy.remainingHp = enemy.hp;
+  player.remainingHp = player.hp;
+  enemyGauge.style.width = 100 + '%'
+  playerGauge.style.width = 100 + '%'
+  // 最初からゲージが赤く表示される
+  // enemyGauge.classList.add('enemy-gauge');
+  // playerGauge.classList.add('player-gauge');
+  enemyGauge.style.backgroundColor = 'green';
+  playerGauge.style.backgroundColor = 'green';
+  enemyHp.textContent = enemy.remainingHp + '/'+ enemy.hp
+  playerHp.textContent = player.remainingHp + '/'+ player.hp
+  playerAttackBtn.disabled = true;
+  enemyAttackBtn.disabled = false;
 }
-const hpGaugeReduce = () =>{
-  enemyHp.textContent = leftEnemyHp + '/'+ enemy.hp
-  enemyGauge.classList.add('enemy-hp-gauge')
+const calculateDamage = () => {
+  //level差でダメージ幅変えたい
+  const minDamage = 30;
+  const maxDamage = 50;
+  damage = minDamage + Math.floor(Math.random() * (maxDamage + 1 -minDamage));
+}
+const updateEnemyHp = () => {
+  enemy.remainingHp -= damage;
+  //  remainEnemyHp = enemy.hp -= damage;
+  //  にすると、最大Hpの表示がremainEnemyHpになる。なんで？
+  //  remainEnemyHp = enemy.hp であり、そこから-= damageを処理するから
+}
+const updatePlayerHp = () => {
+  player.remainingHp -= damage;
+}
+const updateEnemyHpGauge = () => {
+  //cssに動きをつけているのがわかったほうがいい
+  //cssなのか数値を変更しているのか
+  enemyHp.textContent = enemy.remainingHp + '/'+ enemy.hp
+  enemyGauge.classList.add('enemy-gauge')
   //enemyGaugeのwidth調整
-  enemyGauge.style.width =  leftEnemyHp / enemy.hp * 100;
+  let remainEnemyHpPercentage = enemy.remainingHp / enemy.hp * 100;
+  enemyGauge.style.width = remainEnemyHpPercentage + '%';
+  if(remainEnemyHpPercentage < 0){
+    enemyGauge.style.width = 0;
+  }
+  // Change Color EnemyHpGauge
+  if (remainEnemyHpPercentage < 20){
+    enemyGauge.style.backgroundColor = 'red';
+  } else if (remainEnemyHpPercentage < 50){
+    enemyGauge.style.backgroundColor = 'yellow';
+  }
+
 }
+const updatePlayerHpGauge = () => {
+  //cssに動きをつけているのがわかったほうがいい
+  //cssなのか数値を変更しているのか
+  playerHp.textContent = player.remainingHp + '/'+ player.hp
+  playerGauge.classList.add('player-gauge')
+  //playerGaugeのwidth調整
+  let remainPlayerHpPercentage = player.remainingHp / player.hp * 100;
+  playerGauge.style.width = remainPlayerHpPercentage + '%';
+  if(remainPlayerHpPercentage < 0){
+    playerGauge.style.width = 0;
+  }
+  // Change Color PlayerHpGauge
+  if (remainPlayerHpPercentage < 20){
+    playerGauge.style.backgroundColor = 'red';
+  } else if (remainPlayerHpPercentage < 50){
+    playerGauge.style.backgroundColor = 'yellow';
+  }
+
+}
+
+const playerTurnEnd = () => {
+  enemyAttackBtn.disabled = true;
+  playerAttackBtn.disabled = false;
+}
+const enemyTurnEnd = () => {
+  enemyAttackBtn.disabled = false;
+  playerAttackBtn.disabled = true;
+}
+const processKillEnemy = () => {
+  if(enemy.remainingHp <= 0) {
+    enemyHp.textContent = 0 + '/'+ enemy.hp
+    alert('You Win!');
+    start();
+    return;
+  }
+}
+const processKilledPlayer = () => {
+  player.textContent = 0 + '/'+ player.hp
+  alert('You Lose!');
+  start();
+  return;
+}
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+start();
 
-//Atack
-playerAttackBtn.addEventListener('click', (e) => {
-  //attack Damege
-  attackDamege();
-  //attack Damege Motion
-  hpGaugeReduce();
+//Player Turn
+enemyAttackBtn.addEventListener('click', (e) => {
+  //attack enemy
+  calculateDamage();
+  //attack Damage Motion
+  //ダメージを与えたのをimgに動きをつけることで表現したい
   //HP ReduceMotion
-
+  //leftHpが減少していくのをleftHpに動きをつけることで表現したい
+    updateEnemyHp();
+  
   //HP Gauge Motion
-  //turn Switch
+  updateEnemyHpGauge();
+  //HPが０になったときの処理
+  if(enemy.remainingHp <= 0) {
+    processKillEnemy();
+    //turn Switch
+  } else {
+    playerTurnEnd();
+  }
 });
 
 
 
+//Enemy Turn
+playerAttackBtn.addEventListener('click', (e) => {
+  //receive damage
+  calculateDamage();
+  //receive Damage Motion
+  //ダメージを受けたのをimgに動きをつけることで表現したい
+  //HP ReduceMotion
+  //leftHpが減少していくのをleftHpに動きをつけることで表現したい
+    updatePlayerHp();
+  //HP Gauge Motion
+  updatePlayerHpGauge();
+  //HPが０になったときの処理
+  if(player.remainingHp <= 0) {
+    processKilledPlayer();
+  } else {
+    //turn Switch
+    enemyTurnEnd();
+  }
+  });
 
-//Recive Damege
 
-//Recive Damege Motion
+////////////コメント///////////////////////
+// なんで動かないか自分で探す
+// 検証機能でエラーチェック
 
-//HP Gauge Motion
+// function 動詞＋名詞
+// コードを読んだ時に文章のように理解できることが理想
 
-
-//Turn Switch
-
+// attackdamage => attaccienemys
